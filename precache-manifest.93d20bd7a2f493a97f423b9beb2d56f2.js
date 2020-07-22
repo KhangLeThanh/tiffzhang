3d3bacd7f4cc68f1507a39336b6cb6e8a0c894cc
Khang Le Thanh <khang.lethanh@student.tut.fi>
2020-07-22 21:18:23 +0300

    Updates

---
+++ precache-manifest.93d20bd7a2f493a97f423b9beb2d56f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802bd48a5573587355eba14f15cf479c",
+    "url": "/tiffzhang/index.html"
+  },
+  {
+    "revision": "485f4ea16d5e6c69d641",
+    "url": "/tiffzhang/static/css/main.76e3f69a.chunk.css"
+  },
+  {
+    "revision": "184b9aab0abd8f4cd545",
+    "url": "/tiffzhang/static/js/2.9f64134f.chunk.js"
+  },
+  {
+    "revision": "6293df48bee467bd59085ae3c63b1ec5",
+    "url": "/tiffzhang/static/js/2.9f64134f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485f4ea16d5e6c69d641",
+    "url": "/tiffzhang/static/js/main.07576c1a.chunk.js"
+  },
+  {
+    "revision": "2a556aad3bb1e4d67ee7",
+    "url": "/tiffzhang/static/js/runtime-main.a0ee0217.js"
+  }
+]);
